4a9da98c751371bcacdb48537140334b853dbfe3
sirysiu <sirisew@gmail.com>
2024-10-31 13:16:26 -0400

    made the contract classes

--- car-dealership/src/main/java/com/pluralsight/car/Contract.java
+++ car-dealership/src/main/java/com/pluralsight/car/Contract.java
@@ -1,4 +1,50 @@
 package com.pluralsight.car;
 
 public class Contract {
+    protected String date;
+    protected String customerName;
+    protected String customerEmail;
+    protected boolean vehicleSold;
+
+    public Contract(String date, String customerName, String customerEmail, boolean vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    double totalPrice;
+    double monthlyPayment;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean isVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(boolean vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
 }
